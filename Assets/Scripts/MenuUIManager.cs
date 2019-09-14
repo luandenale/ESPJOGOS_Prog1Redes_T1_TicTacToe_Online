8813053455023d21cb3c472e649acd8071ae1822
luandenale <luandenale@hotmail.com>
2019-09-14 19:12:23 -0300

    Started refactoring main menu animations (and menus)

--- Assets/Scripts/MenuUIManager.cs
+++ Assets/Scripts/MenuUIManager.cs
@@ -12,28 +12,38 @@ public class MenuUIManager : MonoBehaviour
         _menuAnimator = GetComponent<Animator>();
     }
 
+    public void LocalSelected()
+    {
+
+    }
+
+    public void OnlineSelected()
+    {
+
+    }
+
     public void V1Start()
     {
-        _menuAnimator.SetTrigger("1v1 Selected");
-        GameManager.instance.currentState = GameStates.STARTING;
+        // _menuAnimator.SetTrigger("1v1 Selected");
+        // GameManager.instance.currentState = GameStates.STARTING;
     }
 
     public void VCpuSelected()
     {
-        _menuAnimator.SetBool("Back To Menu", false);
-        _menuAnimator.SetBool("1vCpu Selected", true);
+        // _menuAnimator.SetBool("Back To Menu", false);
+        // _menuAnimator.SetBool("1vCpu Selected", true);
     }
 
     public void BackToMenu()
     {
-        _menuAnimator.SetBool("Back To Menu", true);
-        _menuAnimator.SetBool("1vCpu Selected", false);
+        // _menuAnimator.SetBool("Back To Menu", true);
+        // _menuAnimator.SetBool("1vCpu Selected", false);
     }
 
     public void VCpuStart(int p_difficulty)
     {
-        _menuAnimator.SetTrigger("1vCpu Start");
-        GameManager.instance.Difficulty = p_difficulty;
-        GameManager.instance.currentState = GameStates.STARTING;
+        // _menuAnimator.SetTrigger("1vCpu Start");
+        // GameManager.instance.Difficulty = p_difficulty;
+        // GameManager.instance.currentState = GameStates.STARTING;
     }
 }
